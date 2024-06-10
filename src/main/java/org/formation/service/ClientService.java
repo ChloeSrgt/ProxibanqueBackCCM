d5b46b353a948a71eac1f0c152b98e6e53953cac
clara pecot <clara.pecot44@gmail.com>
2024-06-10 14:27:36 +0200

    transfer service and client service commented

--- src/main/java/org/formation/service/ClientService.java
+++ src/main/java/org/formation/service/ClientService.java
@@ -8,15 +8,21 @@ import org.formation.dto.AccountsDTO;
 import org.formation.model.Client;
 
 public interface ClientService {
+	// Display a list of all clients
 	List<Client> getAll();
 
+	// Save a client
 	Client save(Client client);
-
+	
+	// Display a client according to its id
 	Optional<Client> getClientById(long id);
 
+	// Delete a client according to its id
 	void deleteById(long id);
 
+	// Update a client
 	Client update(Client client);
 
+	// Display the different accounts of a client, according to its id
 	AccountsDTO getAccountsByClientId(long id);
 }
--- src/main/java/org/formation/service/TransferService.java
+++ src/main/java/org/formation/service/TransferService.java
@@ -1,5 +1,6 @@
 package org.formation.service;
 
 public interface TransferService {
+	// Money transfer from one account to another, according to the id
 	boolean transfer(double amount, long fromAccountId, long toAccountId);
 }
